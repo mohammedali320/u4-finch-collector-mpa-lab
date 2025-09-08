6c211d7fc292fde0b88f2a141cba6cfde7ab3b75
Mohamed Ali Kadhem <mhejiy@gmail.com>
2025-09-08 12:55:49 +0300

    doneeeeeeeeee

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bird
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+def bird_index(request):
+    birds = Bird.objects.all() 
+    return render(request, 'birds/index.html', {'birds': birds})
 
 def home(request):
     return render(request, 'home.html')
@@ -7,21 +13,19 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-class Bird:
-    def __init__(self, name, species, description, age):
-        self.name = name
-        self.species = species
-        self.description = description
-        self.age = age
+def bird_detail(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html', {'bird': bird})
 
+class BirdCreate(CreateView):
+    model = Bird
+    fields = '__all__'
+    # success_url = '/birds/'
 
-birds = [
-    Bird('Tweety', 'Canary', 'Yellow and chirpy.', 1),
-    Bird('Koko', 'Parrot', 'Loves to mimic sounds.', 4),
-    Bird('Hoot', 'Owl', 'Quiet at day, active at night.', 3),
-    Bird('Sky', 'Blue Jay', 'Bright blue feathers.', 2)
-]
-# views.py
+class BirdUpdate(UpdateView):
+    model = Bird
+    fields = ['species', 'description', 'age']
 
-def bird_index(request):
-    return render(request, 'birds/index.html', {'birds': birds})
+class BirdDelete(DeleteView):
+    model = Bird
+    success_url = '/birds/'
